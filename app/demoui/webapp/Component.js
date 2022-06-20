4ce8aaf138c4aa921e09c96b3ea5717eadbf1f36
Srikrishna Charan Kalavacharla <srikrishna.charan.kalavacharla@sap.com>
2022-06-20 11:24:00 +0000

    project files

---
+++ app/demoui/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/fe/core/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("demoui.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
